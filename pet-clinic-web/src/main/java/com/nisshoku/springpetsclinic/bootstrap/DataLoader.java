2585001f04536bfb0fe35a590271e2fff116e82f
takacchi <uchiha.takahiro@gmail.com>
2018-11-17 17:55:33 +0100

    Refactored Map impl tp abstarct ID generation. Closes #23

--- pet-clinic-web/src/main/java/com/nisshoku/springpetsclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nisshoku/springpetsclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Takahiro");
         owner1.setLastName("Hayami");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Momo");
         owner2.setLastName("Hirai");
 
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Ryuu");
         vet1.setLastName("Tashii");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Misaki");
         vet2.setLastName("Sakura");
 
