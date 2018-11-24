d27a4a417ae049ddf0c383e3c12b9fc9aa058399
takacchi <uchiha.takahiro@gmail.com>
2018-11-24 16:00:18 +0100

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/nisshoku/springpetsclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.nisshoku.springpetsclinic.services;
+
+import com.nisshoku.springpetsclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
