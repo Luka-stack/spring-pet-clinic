5bbc80f60cba8869a85499b6798b871ccec23857
takacchi <uchiha.takahiro@gmail.com>
2018-11-24 15:52:52 +0100

    Added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-web/src/main/java/com/nisshoku/springpetsclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nisshoku/springpetsclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.nisshoku.springpetsclinic.bootstrap;
 
 import com.nisshoku.springpetsclinic.model.Owner;
+import com.nisshoku.springpetsclinic.model.Pet;
 import com.nisshoku.springpetsclinic.model.PetType;
 import com.nisshoku.springpetsclinic.model.Vet;
 import com.nisshoku.springpetsclinic.services.OwnerService;
@@ -9,6 +10,8 @@ import com.nisshoku.springpetsclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,12 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Takahiro");
         owner1.setLastName("Hayami");
+        owner1.setAddress("123 Sakura");
+        owner1.setCity("Osaka");
+        owner1.setTelephone("123456789");
+
+        Pet takasPet = new Pet();
+        takasPet.setPetType(savedDogPetType);
+        takasPet.setOwner(owner1);
+        takasPet.setBirthDate(LocalDate.now());
+        takasPet.setName("Inu");
+        owner1.getPets().add(takasPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Momo");
         owner2.setLastName("Hirai");
+        owner1.setAddress("123 Sakura");
+        owner1.setCity("Osaka");
+        owner1.setTelephone("111000222");
+
+        Pet momosPet = new Pet();
+        momosPet.setPetType(savedCatPetType);
+        momosPet.setOwner(owner2);
+        momosPet.setBirthDate(LocalDate.now());
+        momosPet.setName("Nekko");
+        owner2.getPets().add(momosPet);
 
         ownerService.save(owner2);
 
