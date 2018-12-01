3c9815704ea6556cc398c1aeb45619d424ee5882
takacchi <uchiha.takahiro@gmail.com>
2018-12-01 16:51:49 +0100

    Implemented JPA Visit Service and added loader data. Closes #47

---
+++ pet-clinic-data/src/main/java/com/nisshoku/springpetsclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,51 @@
+package com.nisshoku.springpetsclinic.services.springdatajpa;
+
+import com.nisshoku.springpetsclinic.model.Visit;
+import com.nisshoku.springpetsclinic.repositories.VisitRepository;
+import com.nisshoku.springpetsclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+
+        Set<Visit> visits = new HashSet<>();
+
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-web/src/main/java/com/nisshoku/springpetsclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nisshoku/springpetsclinic/bootstrap/DataLoader.java
@@ -1,11 +1,7 @@
 package com.nisshoku.springpetsclinic.bootstrap;
 
 import com.nisshoku.springpetsclinic.model.*;
-import com.nisshoku.springpetsclinic.services.OwnerService;
-import com.nisshoku.springpetsclinic.services.PetTypeService;
-import com.nisshoku.springpetsclinic.services.SpecialityService;
-import com.nisshoku.springpetsclinic.services.VetService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nisshoku.springpetsclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,14 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
-    @Autowired
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialityService specialityService) {
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -91,6 +88,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(momosPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(momosPet);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners...");
 
