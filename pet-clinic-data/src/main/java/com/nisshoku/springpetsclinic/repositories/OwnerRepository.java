b543d7c7ad9fbad90eee367535b2c6b7fe67245c
takacchi <uchiha.takahiro@gmail.com>
2018-12-28 13:25:26 +0100

    Implemented Find Owner functionality. Closes #54

--- pet-clinic-data/src/main/java/com/nisshoku/springpetsclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/nisshoku/springpetsclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.nisshoku.springpetsclinic.repositories;
 import com.nisshoku.springpetsclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/nisshoku/springpetsclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/nisshoku/springpetsclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.nisshoku.springpetsclinic.services;
 
 import com.nisshoku.springpetsclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
